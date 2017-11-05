6f23a61a343d23a3e21cea1ec6b0fc744f25f5ed
vnydev <vineysharma1102@gmail.com>
2017-11-06 00:16:51 +0530

    change the limit of the fetch images

--- public/javascripts/callGoogle.js
+++ public/javascripts/callGoogle.js
@@ -5,7 +5,7 @@ var getImages = function(keyWord){
     console.log("call is going to fetch from google")
     var call = google.list({
         keyword: keyWord,
-        num: 30,
+        num: 15,
         detail: true,
         nightmare: {
             show: false
